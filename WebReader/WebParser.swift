1b7e877f9ab6830916a3c3181e9c9aa9914888bb
CWaYNe <magedesang@gmail.com>
2017-02-16 14:01:55 +0800

    remove some comments

--- WebReader/WebParser.swift
+++ WebReader/WebParser.swift
@@ -14,9 +14,12 @@ var NonSense: [String] = [
     "(adsbygoogle = window.adsbygoogle || []).push({});", "章节缺失、错误举报", "U","看书","请记住本书首发域名",
     "言情小说网手机版阅读网址：","手机版阅读网址：", "：。"
 ]
+
+var NonSense2: [String] = [
+    "請記住本站域名: 黃金屋",
+]
+
 typealias chapterTuple = (text: String?, url: String?)
-
-
 
 class Book {
     
@@ -57,12 +60,9 @@ class DemoBook: Book {
             if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
                 if let doc = Kanna.HTML(html: utf8Text, encoding: .utf8) {
                     if let bookname = doc.at_css("h1"){
-                        let isTraditional: Bool! = UserDefaults.standard.bool(forKey: "isTraditional")
-                        if(isTraditional == true){
-                            self.bookTitle = s2t(text: bookname.text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines))
-                        }else {
-                            self.bookTitle = bookname.text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-                        }
+                        
+                        self.bookTitle = s2t(text: bookname.text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines))
+
                         completion(self.bookTitle!)
                     }
                 }
@@ -78,19 +78,12 @@ class DemoBook: Book {
         Alamofire.request(url).response { response in
             if let data = response.data, let utf8Text = String(data:data, encoding: .utf8) {
                 if let doc = Kanna.HTML(html: utf8Text, encoding: .utf8) {
-                    let isTraditional: Bool! = UserDefaults.standard.bool(forKey: "isTraditional")
                     
-                    if(isTraditional == true){
-                        for link in doc.xpath("//div[contains(@id, 'tbchapterlist')] // td // a") {
-                            chapterInfo = (text:s2t(text:link.text!), url:link["href"]!)
-                            chapterList.append(chapterInfo)
-                        }
-                    }else {
-                        for link in doc.xpath("//div[contains(@id, 'tbchapterlist')] // td // a") {
-                            chapterInfo = (text:link.text!, url:link["href"]!)
-                            chapterList.append(chapterInfo)
-                        }
+                    for link in doc.xpath("//div[contains(@id, 'tbchapterlist')] // td // a") {
+                        chapterInfo = (text:s2t(text:link.text!), url:link["href"]!)
+                        chapterList.append(chapterInfo)
                     }
+
                     completionHandler(chapterList)
                 }
             }
@@ -134,17 +127,12 @@ class DemoBook: Book {
                     }
                     
                     // Compare time with the replacing function inside the above for-loop
-                    for words in NonSense {
-                        if (words == "(adsbygoogle = window.adsbygoogle || []).push({});") {
-                            contentText = contentText.replacingOccurrences(of: "\n\n\n", with: "")
-                            //contentText = contentText.replacingOccurrences(of: "\n\n", with: "\n")
-                        }
+                    for words in NonSense2 {
                         contentText = contentText.replacingOccurrences(of: words, with: "")
                     }
-                    let isTraditional: Bool! = UserDefaults.standard.bool(forKey: "isTraditional")
-                    if (isTraditional == true){
-                        contentText = s2t(text: contentText)!
-                    }
+                    
+                    contentText = s2t(text: contentText)!
+                    
                     completionHandler(contentText, chapterTitle, nextUrl)
                 }
                 
@@ -160,6 +148,28 @@ class DemoBook: Book {
     
     init(bookUrl: String, bookTitle: String) {
         super.init(url: bookUrl, title: bookTitle)
+    }
+}
+
+// Used at searching vc
+func getBookList(url: String, completionHandler: @escaping ([chapterTuple]) -> ()){
+    
+    var bookList: [chapterTuple] = []
+    var bookInfo:(String?, String?) = (nil, nil)
+    
+    Alamofire.request(url).response { response in
+        if let data = response.data, let utf8Text = String(data:data, encoding: .utf8) {
+            if let doc = Kanna.HTML(html: utf8Text, encoding: .utf8) {
+                for link in doc.xpath("//div/following-sibling::div[1] //a") {
+                    if let title = link["title"]{
+                        let correctUrl = "http://tw.hjwzw.com/" + link["href"]!.replacingOccurrences(of: "/Book/", with: "Book/Chapter/")
+                        bookInfo = (text: title, url:correctUrl)
+                        bookList.append(bookInfo)
+                    }
+                }
+                completionHandler(bookList)
+            }
+        }
     }
 }
 
@@ -277,110 +287,3 @@ class UUBook: Book {
     }
 }
 
-
-
-// Used at searching vc
-func getBookList(url: String, completionHandler: @escaping ([chapterTuple]) -> ()){
-    
-    var bookList: [chapterTuple] = []
-    var bookInfo:(String?, String?) = (nil, nil)
-    
-    Alamofire.request(url).response { response in
-        if let data = response.data, let utf8Text = String(data:data, encoding: .utf8) {
-            if let doc = Kanna.HTML(html: utf8Text, encoding: .utf8) {
-                for link in doc.xpath("//div/following-sibling::div[1] //a") {
-                    if let title = link["title"]{
-                        let correctUrl = "http://tw.hjwzw.com/" + link["href"]!.replacingOccurrences(of: "/Book/", with: "Book/Chapter/")
-                        bookInfo = (text: title, url:correctUrl)
-                        bookList.append(bookInfo)
-                    }
-                }
-                completionHandler(bookList)
-            }
-        }
-    }
-}
-
-/*
-
-func getBookChapterList(url: String, domain: String, completionHandler: @escaping ([chapterTuple]) -> () ){
-    
-    var chapterList: [chapterTuple] = []
-    var chapterInfo:(String?, String?) = (nil, nil)
-    
-    Alamofire.request(url).response { response in
-        if let data = response.data, let utf8Text = String(data:data, encoding: .utf8) {
-            if let doc = Kanna.HTML(html: utf8Text, encoding: .utf8) {
-                let isTraditional: Bool! = UserDefaults.standard.bool(forKey: "isTraditional")
-                
-                if(isTraditional == true){
-                    for link in doc.xpath("//div[contains(@class, 'ml-list')] // a") {
-                        chapterInfo = (text:s2t(text:link.text!), url:link["href"]!)
-                        chapterList.append(chapterInfo)
-                    }
-                }else {
-                    for link in doc.xpath("//div[contains(@class, 'ml-list')] // a") {
-                        chapterInfo = (text:link.text!, url:link["href"]!)
-                        chapterList.append(chapterInfo)
-                    }
-                }
-                completionHandler(chapterList)
-            }
-        }
-    }
-}
-
-
-func getChapterContent(url: String, domain: String, completionHandler: @escaping (String, String, String?) -> ()){
-    
-    var contentText: String = ""
-    // Ckeck url completeness before concatenation
-    let fullUrl = "http://" + domain + url
-    var nextUrl: String?
-    var chapterTitle: String!
-    
-    Alamofire.request(fullUrl).response{ response in
-        if let data = response.data, var utf8Text = String(data:data, encoding: .utf8){
-            // refine content presentation
-            utf8Text = utf8Text.replacingOccurrences(of: "<br>", with: "\n")
-            utf8Text = utf8Text.replacingOccurrences(of: "<br/>", with: "\n")
-            utf8Text = utf8Text.replacingOccurrences(of: "<p>", with: "\n")
-            utf8Text = utf8Text.replacingOccurrences(of: "<p/>", with: "\n")
-            if let doc = Kanna.HTML(html: utf8Text, encoding: .utf8) {
-                
-                for txt in doc.xpath("//h3"){
-                    chapterTitle = s2t(text:txt.text)
-                }
-                
-                contentText = chapterTitle + "\n"
-                
-                for content in doc.xpath("//div[contains(@id, 'bookContent')]") {
-                    contentText += content.text!
-                }
-                
-                for url in doc.xpath("//div[contains(@class, 'rp-switch')] //a[contains(@id, 'read_next')]") {
-                    nextUrl = url["href"]
-                }
-                
-                
-                // Compare time with the replacing function inside the above for-loop
-                for words in NonSense {
-                    if (words == "(adsbygoogle = window.adsbygoogle || []).push({});") {
-                        contentText = contentText.replacingOccurrences(of: "\n\n\n", with: "")
-                        //contentText = contentText.replacingOccurrences(of: "\n\n", with: "\n")
-                    }
-                    contentText = contentText.replacingOccurrences(of: words, with: "")
-                }
-                let isTraditional: Bool! = UserDefaults.standard.bool(forKey: "isTraditional")
-                if (isTraditional == true){
-                    contentText = s2t(text: contentText)!
-                }
-                completionHandler(contentText, chapterTitle, nextUrl)
-            }
-            
-        }
-    
-    }
-    
-}
-*/
